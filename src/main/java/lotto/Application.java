92a2c2dc92bef0892260e8176ff0dfb6d7b19f66
Junseung-Ock <junseung0305@naver.com>
2024-11-04 23:01:00 +0900

    수량 및 번호 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import net.bytebuddy.implementation.bytecode.assign.primitive.PrimitiveUnboxingDelegate;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
@@ -11,15 +14,32 @@ public class Application {
     void inputTotalMoney(){
         System.out.println("금액을 입력해 주세요.");
         int totalMoney = scan.nextInt();
-        countNumber(totalMoney);
+        System.out.printf("%d개를 구매했습니다.", countNumber(totalMoney));
+        System.out.println();
+        printRandomNumbers(totalMoney);
     }
 
-    void countNumber(int totalMoney){
-        System.out.printf("%d개를 구매했습니다.", totalMoney/1000);
+    int countNumber(int totalMoney){
+        return totalMoney/1000;
+    }
+
+    void printRandomNumbers(int totalMoney){
+        for(int i=0; i<countNumber(totalMoney); i++) {
+           List<Integer> number = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(number);
+            System.out.print("[");
+           for(int j =0 ;j<5 ;j++){
+                System.out.printf("%d, ", number.get(j));
+            }
+            System.out.printf("%d", number.get(5));
+            System.out.print("]");
+           System.out.println();
+        }
     }
 
     void run (){
         inputTotalMoney();
+
     }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
